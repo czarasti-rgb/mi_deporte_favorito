204b7fc0a8ca6238f9081742136f50083de169ea
Carlos Zarasti <c.zarasti@estom.ecotec.edu.ec>
2026-09-05 23:26:29 -0500

    Implementación del modelo de deportes

---
+++ lib/models/sport.dart
@@ -0,0 +1,13 @@
+class Sport {
+  final String name;
+  final String description;
+  final String image;
+  final String category;
+
+  Sport({
+    required this.name,
+    required this.description,
+    required this.image,
+    required this.category,
+  });
+}
